f5735a5d557aaf1b847196356ff621dcab9acea8
Muzammil Shaikh <hello.imshykh@gmail.com>
2026-06-21 20:18:14 +0500

    Uploaded some supporting files

---
+++ Aura Watches App/js/script.js
@@ -0,0 +1,103 @@
+// JavaScript Document
+
+
+
+//header
+function head(){
+	"use strict";
+	document.getElementById('head').innerHTML='<!--navBar Start--><nav class="navbar navbar-inverse navbar-fixed-top hidden-xs" id="my-navbar"><div class="container"><div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a href="index.html" class="navbar-brand bdrright shake" style="color:white">Aura Watches</a></div><!-- Navbar Header--><div class="collapse navbar-collapse" id="navbar-collapse"><a href="downloads/complete.docx" class="btn navbar-btn navbar-right color-btn hidden-xs" data-toggle="tooltip" data-placement="bottom" title="Complete Download"><span class="glyphicon glyphicon-download-alt font30"></span></a><a href="download.html" class="btn navbar-btn navbar-right color-btn hidden-xs" data-toggle="tooltip" data-placement="bottom" title="Download Page" style="margin-right: 5px;"><span class="glyphicon glyphicon-download font30"></span></a><ul class="nav navbar-nav navbar-right"><li><a href="index.html" >Home</a></li><li><a href="Products.html" class="dropdown-toggle" data-toggle="dropdown">Products<b class="caret"></b></a><ul class="dropdown-menu multi-level"><li><a href="index.html#brands">Top Brands</a></li><li><a href="index.html#top">Hot This Week</a></li><li class="divider"></li><li><a href="technology.html">Technology 2017</a></li><li class="divider"></li><li class="dropdown-submenu"><a href="" class="dropdown-toggle" data-toggle="dropdown">Catagories</a><ul class="dropdown-menu"><li class="dropdown-submenu"><a href="" class="dropdown-toggle" data-toggle="dropdown">Mens Watches</a><ul class="dropdown-menu"><li class="dropdown-submenu"><li><a href="armani.html">Armani</a></li><li><a href="Diesel.html">Diesel</a></li><li><a href="Hugo Boss.html">Hugo Boss</a></li><li><a href="Michael Kors.html">Michael Kors</a></li></li></ul></li><li class="dropdown-submenu"><a href="" class="dropdown-toggle" data-toggle="dropdown">Women Watches</a><ul class="dropdown-menu"><li class="dropdown-submenu"><li><a href="Olivia Burton.html">Olivia Burton</a></li><li><a href="Dkny.html">Dkny</a></li><li><a href="Obaku.html">Obaku</a></li><li><a href="Braun.html">Braun</a></li></li></ul></li><li class="dropdown-submenu"><a href="" class="dropdown-toggle" data-toggle="dropdown">Sports</a><ul class="dropdown-menu"><li class="dropdown-submenu"><li><a href="Shark.html">Shark Sports</a></li><li><a href="Casio.html">Casio G-Shock</a></li><li><a href="Tag Heuer.html">Tag Heuer</a></li><li><a href="U Boat.html">U Boat</a></li></li></ul></li><li class="dropdown-submenu"><a href="" class="dropdown-toggle" data-toggle="dropdown">Wall Clocks</a><ul class="dropdown-menu"><li class="dropdown-submenu"><li><a href="Ajanta.html">Ajanta</a></li><li><a href="Seiko.html">Seiko</a></li></ul></li></ul></li></ul></li><li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Services<b class="caret"></b></a><ul class="dropdown-menu multi-level"><li><a href="store.html">Store Locator</a></li><li><a href="About-us.html">About Us</a></li><li><a href="contact-us.html">Contact Us</a></li><li class="divider"></li><li><a href="warranty.html">Warenty Exchenge</a></li></ul></li><li class="hidden-xs bdrright"><a href="#courses">&nbsp;</a></li><li><a href="#courses" class="hidden-xs">&nbsp;</a></li></ul></div></div><!-- End Container--></nav>   <!--navBar Start--><nav class="navbar navbar-inverse navbar-top visible-xs" id="my-navbar"><div class="container"><div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#xs-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a href="index.html" class="navbar-brand bdrright shake" style="color:white">Aura Watches</a></div><!-- Navbar Header--><div class="collapse navbar-collapse" id="xs-collapse"><a href="downloads/complete.docx" class="btn navbar-btn navbar-right color-btn hidden-xs" data-toggle="tooltip" data-placement="bottom" title="Complete Download"><span class="glyphicon glyphicon-download-alt font30"></span></a><a href="download.html" class="btn navbar-btn navbar-right color-btn hidden-xs" data-toggle="tooltip" data-placement="bottom" title="Download Page" style="margin-right: 5px;"><span class="glyphicon glyphicon-download font30"></span></a><ul class="nav navbar-nav navbar-right"><li><a href="index.html" >Home</a></li><li><a href="Products.html" class="dropdown-toggle" data-toggle="dropdown">Products<b class="caret"></b></a><ul class="dropdown-menu multi-level"><li><a href="index.html#brands">Top Brands</a></li><li class="divider"></li><li><a href="technology.html">Technology 2017</a></li><li class="divider"></li><li class="dropdown-submenu"><a href="products.html" class="dropdown-toggle" data-toggle="dropdown">Catagories</a><ul class="dropdown-menu"><li class="dropdown-submenu"><a href="mens.html" class="dropdown-toggle" data-toggle="dropdown">Mens Watches</a><ul class="dropdown-menu"><li class="dropdown-submenu"><li><a href="armani.html">Armani</a></li><li><a href="#">HugoBoss</a></li><li><a href="#">Armanis</a></li><li><a href="#">Armanis</a></li></li></ul></li><li class="dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Mens Watches</a><ul class="dropdown-menu"><li class="dropdown-submenu"><li><a href="#">Gucci</a></li><li><a href="#">Gucci</a></li><li><a href="#">Gucci</a></li><li><a href="#">Gucci</a></li></li></ul></li><li class="dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Wall Clocks</a><ul class="dropdown-menu"><li class="dropdown-submenu"><li><a href="#">Names</a></li><li><a href="#">Names</a></li><li><a href="#">Names</a></li><li><a href="#">Names</a></li></li></ul></li><li class="dropdown-submenu"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Sports</a><ul class="dropdown-menu"><li class="dropdown-submenu"><li><a href="#">Named</a></li><li><a href="#">Named</a></li><li><a href="#">Named</a></li><li><a href="#">Named</a></li></li></ul></li></ul></li></ul></li><li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Services<b class="caret"></b></a><ul class="dropdown-menu multi-level"><li><a href="store.html">Store Locator</a></li><li><a href="About-us.html">About Us</a></li><li><a href="contact-us.html">Contact Us</a></li><li class="divider"></li><li><a href="warranty.html">Warenty Exchenge</a></li></ul></li><li class="hidden-xs bdrright"><a href="#courses">&nbsp;</a></li><li><a href="#courses" class="hidden-xs">&nbsp;</a></li></ul></div></div><!-- End Container--></nav>   <!-- End navbar -->';
+}
+
+//footer
+function foot(){
+	"use strict";
+	document.getElementById('foot').innerHTML='<!--FOOTER--><div id="footer" class="clearfix hidden-xs"><div class="container"><div class="row"><div class="col-md-3 col-sm-4"><h4>Pages</h4><ul class="padding0"><li><a href="technology.html" data-toggle="tooltip" data-placement="right" title="Technology">Technology</a></li><li><a href="warranty.html" data-toggle="tooltip" data-placement="right" title="Warranty">Warranty Exchenge</a></li><li><a href="about-us.html" data-toggle="tooltip" data-placement="right" title="About-Us">About us</a></li><li><a href="contact-us.html" data-toggle="tooltip" data-placement="right" title="Contact-Us">Contact us</a></li></ul><hr class="hidden-md hidden-lg hidden-sm"></div><div class="col-md-3 col-sm-4"><h4>Top categories</h4><h5>Men</h5><ul><li><a href="Armani.html">Armani</a></li><li><a href="Michael Kors.html">Michael Kors</a></li></ul><h5>Women</h5><ul><li><a href="Olivia Burton.html">Olivia Burton</a></li><li><a href="Dkny.html">Dkny</a></li></ul><h5>Wall clocks</h5><ul><li><a href="Ajanta.html">Ajanta</a></li></ul></div><div class="col-md-3 col-sm-4"><h4>Where to find us</h4><p><strong>Aptech</strong><br>Ghulistan-e-johar,<br>block#19,<br>karachi,<br>Pakistan.<br></p></div><div class="col-md-3 visible-lg visible-md visible-xs"><h4>Get the news</h4><p class="text-muted">Subscribe And Stay Updated By Email, Do What Best Is the World is Doing</p><form><div class="input-group"><input type="text" class="form-control"><span class="input-group-btn"><button class="btn btn-default" type="button">Subscribe!</button></span></div></form><hr><h4>Stay in touch</h4><div class="row margin0auto"></div><div class="col-sm-3 col-xs-3"><a href="#"><img class="img-responsive grayScale" src="images/Social Icons/Facebook.png" alt="facebook"></a></div><div class="col-sm-3 col-xs-3"><a href="#"><img class="img-responsive grayScale" src="images/Social Icons/Twitter.png" alt="facebook"></a></div><div class="col-sm-3 col-xs-3"><a href="#"><img class="img-responsive grayScale" src="images/Social Icons/Instagram.png" alt="facebook"></a></div><div class="col-sm-3 col-xs-3"><a href="#"><img class="img-responsive grayScale" src="images/Social Icons/Mail.png" alt="facebook"></a></div></div><div class="row visible-sm"><div class="col-sm-12"><h4>Get the news</h4><p class="text-muted">Subscribe And Stay Updated By Email, Do What Best Is the World is Doing</p><form><div class="input-group"><input type="text" class="form-control"><span class="input-group-btn"><button class="btn btn-default" type="button">Subscribe!</button></span></div></form><hr><h4>Stay in touch</h4><br><div class="row margin0auto"></div><div class="col-sm-3 col-xs-3"><a href="#"><img class="img-responsive grayScale" src="images/Social Icons/Facebook.png" alt="facebook"></a></div><div class="col-sm-3 col-xs-3"><a href="#"><img class="img-responsive grayScale" src="images/Social Icons/Twitter.png" alt="facebook"></a></div><div class="col-sm-3 col-xs-3"><a href="#"><img class="img-responsive grayScale" src="images/Social Icons/Instagram.png" alt="facebook"></a></div><div class="col-sm-3 col-xs-3"><a href="#"><img class="img-responsive grayScale" src="images/Social Icons/Mail.png" alt="facebook"></a></div></div></div></div></div></div><!--FOOTER END--><!--COPYRIGHT--><div id="copyright"><div class="container"><div class="col-md-6"><p class="pull-left">© 2017 Aura Watches</p></div><div class="col-md-6"><p class="pull-right">Created By <a href="#">Muzammil & Team</a></p></div></div></div><!--COPYRIGHT END-->';
+}
+
+function Tooltip(){
+	"use strict";
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();   
+	});
+}
+
+function show() {
+	"use strict";
+    var x = document.getElementById('row');
+	var y = document.getElementById('btn');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+		y.style.display = 'none';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+	window.onload = function(){
+	"use strict";
+	printTime();
+	};
+
+	function printTime() {
+		"use strict";
+		var d = new Date();
+		var hours = d.getHours();
+		var mins = d.getMinutes();
+		var secs = d.getSeconds();
+		var day = d.getDay();
+		var date = d.getDate();
+		var month = d.getMonth();
+		var year = d.getFullYear();
+
+			switch (day){
+
+				case 0:
+					day = "Sunday";
+					break;
+				case 1:
+					day = "Monday";
+					break;
+				case 2:
+					day = "Tuesday";
+					break;
+				case 3:
+					day = "Wednesday";
+					break;
+				case 4:
+					day = "Thursday";
+					break;
+				case 5:
+					day = "Friday";
+					break;
+				case 6:
+					day = "Saturday";
+					break;
+			   }
+
+
+			if(hours<10){
+				hours = "0" + hours;
+			}
+			if(mins<10){
+
+				mins = "0" + mins;
+			}
+			if(secs<10){
+
+				secs = "0" + secs;
+			}
+			if(hours>12){
+				hours= hours - 12;
+			}
+			if(hours===0){
+				hours= hours + 12;
+			}
+
+	   month = month + 1; document.getElementById("test").innerHTML = hours+":"+mins+":"+secs;
+	}
+	setInterval(printTime, 1000);
+
+head();
+foot();
+Tooltip();
+show();
